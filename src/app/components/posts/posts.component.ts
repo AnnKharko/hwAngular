d36a85d44690111bf0399bf16758feefcf133b85
Ann <kharko2112.mechmat@gmail.com>
2021-01-28 15:29:48 +0200

    hw4++

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Post} from '../../models/Post';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-posts',
@@ -11,10 +11,9 @@ export class PostsComponent implements OnInit {
  posts: Post[];
  id: number;
 
- constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+ constructor(private activatedRoute: ActivatedRoute) {
     this.activatedRoute.data.subscribe(value => this.posts = value.postData);
     this.id = history.state.id;
-    console.log(history);
  }
 
   ngOnInit(): void { }
