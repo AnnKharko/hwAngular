ae9ef2b07386a5dfc72f1c3fc6d34228e0678f47
Ann <kharko2112.mechmat@gmail.com>
2021-02-02 21:04:20 +0200

    hw5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+import {User} from '../models/User';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hwAngular';
+
+
+  users: User[] = [];
+
+  user = new User('', +'', '');
+
+
+
+  name = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  age = new FormControl(+'', [Validators.required, Validators.max(116), Validators.min(16)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age,
+    email: this.email
+  });
+
+
+
+  submit(form: NgForm): void {
+    console.log(form);
+    const user = new User(form.controls.name.value, form.controls.age.value, form.controls.email.value );
+    this.users.push(user);
+    console.log(this.users);
+
+  }
+
+
+  saveUser(myForm: FormGroup): void {
+    console.log(myForm);
+    const user = new User(myForm.controls.name?.value, myForm.controls.age?.value, myForm.controls.email?.value);
+    this.users.push(user);
+  }
+
+
 }
